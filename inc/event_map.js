cadee4e4a7c9c7a879bee6cbc29b495ef0d30201
Stephan Plepelits <skunk@xover.mud.at>
2012-06-17 14:11:01 +0200

    Only download new locations

--- inc/event_map.js
+++ inc/event_map.js
@@ -4,9 +4,15 @@ function event_map() {
 }
 
 event_map.prototype.update_callback=function(data) {
+  if(data.last_timestamp)
+    this.last_timestamp=data.last_timestamp;
 }
 
 event_map.prototype.update=function() {
-  ajax("event_map_update", {}, null, this.update_callback.bind(this));
+  var param={};
+  if(this.last_timestamp)
+    param.last_timestamp=this.last_timestamp;
+
+  ajax("event_map_update", param, null, this.update_callback.bind(this));
 }
 
--- inc/event_map.php
+++ inc/event_map.php
@@ -4,7 +4,12 @@ function ajax_event_map_update() {
   $ret=array();
   $last_timestamp='';
 
-  $res=sqlite_query($db, "select * from gps_log where timestamp>datetime('now', '-10 minute') order by session_id, timestamp desc");
+  if(isset($_REQUEST['last_timestamp']))
+    $where="timestamp>'".sqlite_escape_string($_REQUEST['last_timestamp'])."' and timestamp>datetime('now', '-10 minute')";
+  else
+    $where="timestamp>datetime('now', '-10 minute')";
+
+  $res=sqlite_query($db, "select * from gps_log where $where order by session_id, timestamp desc");
   while($elem=sqlite_fetch_array($res, SQLITE_ASSOC)) {
     if($elem['timestamp']>$last_timestamp)
       $last_timestamp=$elem['timestamp'];
