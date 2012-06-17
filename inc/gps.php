2f79733ab1bdbff579938e5015895d78b4f264a1
Stephan Plepelits <skunk@xover.mud.at>
2012-06-17 12:01:57 +0200

    Insert position of a session member into database

--- inc/gps.php
+++ inc/gps.php
@@ -1,4 +1,16 @@
 <?
 function ajax_gps_submit() {
+  global $db;
+
+  $str=array("'".session_id()."'", "datetime('now')");
+
+  foreach(array("longitude", "latitude", "altitude", "speed", "accuracy", "altitudeAccuracy", "heading") as $k) {
+    if(isset($_REQUEST[$k])&&$_REQUEST[$k])
+      $str[]=sqlite_escape_string($_REQUEST[$k]);
+  }
+
+  $str=implode(", ", $str);
+
+  sqlite_query($db, "insert into gps_log values ($str)");
   return true;
 }
