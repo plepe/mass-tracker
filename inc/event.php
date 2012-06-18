42b603911c6c431a9625fdad46ffa3c8b5d91a67
Stephan Plepelits <skunk@xover.mud.at>
2012-06-18 21:14:33 +0200

    Event: Add data['status'] with values current, coming_up and past

--- inc/event.php
+++ inc/event.php
@@ -5,7 +5,7 @@ class event {
 
     $this->id=sqlite_escape_string($id);
 
-    $res=sqlite_query($db, "select * from event where event_id='{$this->id}'");
+    $res=sqlite_query($db, "select *, case when (begin_time<=datetime('now') and end_time>datetime('now')) then 'current' when (begin_time>datetime('now')) then 'coming_up' when (end_time<=datetime('now')) then 'past' end as status from event where event_id='{$this->id}'");
     $this->data=sqlite_fetch_array($res, SQLITE_ASSOC);
   }
 
