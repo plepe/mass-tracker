5256a412fab423d5530895b74020a7b5b1a61de4
Stephan Plepelits <skunk@xover.mud.at>
2012-06-18 17:02:24 +0200

    GPS: Save event_id to gps log entries

--- inc/gps.php
+++ inc/gps.php
@@ -2,7 +2,14 @@
 function ajax_gps_submit() {
   global $db;
 
-  $str=array("'".session_id()."'", "datetime('now')");
+  if(!isset($_SESSION['event_id']))
+    return;
+
+  $str=array(
+    "'".session_id()."'",
+    "'".sqlite_escape_string($_SESSION['event_id'])."'",
+    "datetime('now')"
+  );
 
   foreach(array("longitude", "latitude", "altitude", "speed", "accuracy", "altitudeAccuracy", "heading") as $k) {
     if(isset($_REQUEST[$k])&&$_REQUEST[$k])
@@ -12,5 +19,6 @@ function ajax_gps_submit() {
   $str=implode(", ", $str);
 
   sqlite_query($db, "insert into gps_log values ($str)");
+
   return true;
 }
