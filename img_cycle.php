1f7d156c1ed11e00ae876651eaf4adda724c8e86
Stephan Plepelits <skunk@xover.mud.at>
2012-11-04 22:15:35 +0100

    Img Cycle: Use SVG (instead of PNG)

--- img_cycle.php
+++ img_cycle.php
@@ -10,13 +10,7 @@ if(isset($_REQUEST['color2'])&&preg_match("/^#[0-9A-Fa-f]{6}$/", $_REQUEST['colo
 $tmpfile=tempnam("/tmp/", "pos_cycle");
 $svg=file_get_contents("img/pos_cycle.svg");
 $svg=strtr($svg, array("COLOR1"=>$color1, "COLOR2"=>$color2));
-file_put_contents($tmpfile, $svg);
 
-system("convert -background none SVG:$tmpfile PNG:$tmpfile.png");
-
-Header("Content-Type: image/png");
+Header("Content-Type: image/svg+xml");
 Header("Cache-Control: max-age=86400, public", true);
-print file_get_contents("$tmpfile.png");
-
-unlink("$tmpfile");
-unlink("$tmpfile.png");
+print $svg;
