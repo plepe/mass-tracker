af561fdad56e38a8d42ffc117fa1b97b50932422
Stephan Plepelits <skunk@xover.mud.at>
2012-11-05 04:58:28 +0100

    Page: modules may use page.php to show additional pages

---
+++ page.php
@@ -0,0 +1,22 @@
+<?php
+include "conf.php";
+include "modulekit/loader.php"; /* loads all php-includes */
+Header("Content-Type: text/html; charset=utf-8");
+session_start();
+
+?>
+<html>
+  <head>
+    <title><?=$title?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<script type='text/javascript' src='lib/php.default.min.js'></script>
+    <?php print modulekit_to_javascript(); /* pass modulekit configuration to JavaScript */ ?>
+    <?php print modulekit_include_js(); /* prints all js-includes */ ?>
+    <?php print modulekit_include_css(); /* prints all css-includes */ ?>
+  </head>
+  <body>
+<?php 
+if($_REQUEST['page']) {
+  print call_user_func("page_{$_REQUEST['page']}", $_REQUEST);
+}
+?>
