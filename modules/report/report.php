b647c2e0bf312037531224e27d771f328d3c7fd9
Stephan Plepelits <skunk@xover.mud.at>
2012-11-06 16:24:52 +0100

    Report: Include reports in event_map_update

--- modules/report/report.php
+++ modules/report/report.php
@@ -1,4 +1,7 @@
 <?
+global $report_fields;
+$report_fields=array("longitude", "latitude", "comment", "file_data");
+
 class Report {
   function __construct($param) {
     $this->event_id=$_SESSION['event_id'];
@@ -7,6 +10,7 @@ class Report {
 
   function save($post_data) {
     global $db;
+    global $report_fields;
     global $ajax_timestamp;
     $ajax_timestamp->setTimezone(new DateTimeZone('UTC'));
 
@@ -18,7 +22,7 @@ class Report {
       "'".$ajax_timestamp->format("Y-m-d H:i:s")."'",
     );
 
-    foreach(array("longitude", "latitude", "comment", "file_data") as $k) {
+    foreach($report_fields as $k) {
       if(isset($post_data[$k])&&$post_data[$k])
 	$str[]="'".sqlite_escape_string($post_data[$k])."'";
       else
@@ -37,3 +41,42 @@ function ajax_report_save($param, $post_data) {
   $report=new Report($param);
   return $report->save($post_data);
 }
+
+function report_update_send($ret, $param) {
+  global $db;
+
+  if(isset($param['time_shift'])) {
+    $now="'now', '{$param['time_shift']} second'";
+    $where[]="timestamp<datetime($now)";
+  }
+  else
+    $now="'now'";
+
+  if(isset($param['last_timestamp']))
+    $where[]="timestamp>'".sqlite_escape_string($param['last_timestamp'])."' and timestamp>datetime($now, '-10 minute')";
+  elseif(isset($param['all'])&&$param['all'])
+     ;
+  else
+    $where[]="timestamp>datetime($now, '-10 minute')";
+
+  if(sizeof($where))
+    $where="where ".implode(" and ", $where);
+
+  $res=sqlite_query($db, "select * from report $where order by tracker_id, timestamp asc");
+  while($elem=sqlite_fetch_array($res, SQLITE_ASSOC)) {
+    global $report_fields;
+
+    $d=array();
+    foreach($report_fields as $k)
+      $d[$k]=$elem[$k];
+
+    $d['timestamp']=$elem['timestamp'];
+    $d['type']="report";
+
+    $ret[$elem['tracker_id']][]=$d;
+  }
+
+  return $ret;
+}
+
+register_hook("update_send", "report_update_send");
