2439df14e18d883abb2146c16c324c6d97aa9386
Stephan Plepelits <skunk@xover.mud.at>
2012-11-06 07:21:02 +0100

    Report: show a form, call save() when done

--- modules/report/report.js
+++ modules/report/report.js
@@ -1,11 +1,46 @@
+var report_form={
+  'comment': {
+    'type': "text",
+    'name': "Kommentar"
+  }
+};
+var report_display;
+
 function report_init(displays) {
-  var d=new Display("report", { title: "", type: "large", unit: "" });
-  d.show(displays);
-  d.set_value("<input type='button' value='Berichte!' onClick='report_start()'>");
+  report_display=new Display("report", { title: "", type: "large", unit: "" });
+  report_display.show(displays);
+  report_reset();
+}
+
+function report_reset() {
+  report_display.set_value("<input type='button' value='Berichte!' onClick='report_start()'>");
+}
+
+function Report() {
+  this.form_dom=document.createElement("form");
+  this.form_dom.id="report_form";
+  this.form_dom.onsubmit=this.save.bind(this);
+
+  this.form=new form("report_form", report_form);
+  this.form.show(this.form_dom);
+
+  var input=document.createElement("input");
+  input.type="submit";
+  input.value="Senden";
+  this.form_dom.appendChild(input);
+
+  report_display.set_value(this.form_dom);
+}
+
+Report.prototype.save=function() {
+  report_display.set_value("Senden ...");
+  alert(JSON.stringify(this.form.get_data()));
+
+  return false;
 }
 
 function report_start() {
-  alert("Ok");
+  new Report();
 }
 
 register_hook("init", report_init);
