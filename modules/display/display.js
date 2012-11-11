5271c0075f20413e84886a0b33fafa2a950424c8
Stephan Plepelits <skunk@xover.mud.at>
2012-11-11 14:34:16 +0100

    Display::set_value(): ignore null values

--- modules/display/display.js
+++ modules/display/display.js
@@ -11,29 +11,34 @@ function Display(id, options) {
 
 Display.prototype.set_value=function(value, expanded_value) {
   // remove current content
-  var current=(this.value_node?this.value_node.firstChild:null);
-  while(current) {
-    var next=current.nextSibling;
-    this.value_node.removeChild(current);
-    current=next;
+  if(value!==null) {
+    var current=(this.value_node?this.value_node.firstChild:null);
+    while(current) {
+      var next=current.nextSibling;
+      this.value_node.removeChild(current);
+      current=next;
+    }
   }
 
   // remove current content
-  var current=(this.expanded_node?this.expanded_node.firstChild:null);
-  while(current) {
-    var next=current.nextSibling;
-    this.expanded_node.removeChild(current);
-    current=next;
+  if(expanded_value!=null) {
+    var current=(this.expanded_node?this.expanded_node.firstChild:null);
+    while(current) {
+      var next=current.nextSibling;
+      this.expanded_node.removeChild(current);
+      current=next;
+    }
   }
 
-  switch(this.options.type) {
-    case "datetime":
-      value=strftime("%H:%M", value.getTime()/1000);
-      break;
-    default:
-      if(this.options.format)
-	value=sprintf(this.options.format, value);
-  }
+  if(value)
+    switch(this.options.type) {
+      case "datetime":
+	value=strftime("%H:%M", value.getTime()/1000);
+	break;
+      default:
+	if(this.options.format)
+	  value=sprintf(this.options.format, value);
+    }
 
   if(this.value_node&&value) {
     if(value.nodeType)
@@ -43,7 +48,7 @@ Display.prototype.set_value=function(value, expanded_value) {
   }
 
   if(this.expanded_node&&expanded_value) {
-    if(value.nodeType)
+    if(expanded_value.nodeType)
       this.expanded_node.appendChild(expanded_value);
     else
       this.expanded_node.innerHTML=expanded_value;
