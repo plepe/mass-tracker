549d4cfa6766fa5a22a95003d7cb23b00f920f0e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-02-19 19:09:06 +0100

    client.js: export interface to allow sending messages to the server

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 // Code based on https://gist.github.com/martinsik/2031681
-$(function() {
+function Connection(url) {
   "use strict";
 
   window.webSocket=window.WebSocket || window.MozWebSocket;
@@ -9,15 +9,19 @@ $(function() {
     return;
   }
 
-  var connection=new WebSocket("ws://192.168.0.13:6777");
+  this.connection=new WebSocket(url);
 
-  connection.onopen=function() {
+  this.connection.onopen=function() {
     alert("onopen called");
 
     connection.send("Test message");
   }
 
-  connection.onmessage=function(message) {
+  this.connection.onmessage=function(message) {
     alert("message received: "+message.data);
   }
-});
+}
+
+Connection.prototype.send=function(data) {
+  this.connection.send(JSON.stringify(data));
+}
