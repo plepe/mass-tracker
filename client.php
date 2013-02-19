649fe11873ed53fdb7576bdfe923aaa269a3db18
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-02-19 19:09:46 +0100

    Add a button to send message

--- client.php
+++ client.php
@@ -4,12 +4,16 @@ require "conf.php";
 <!DOCTYPE HTML>
 <html>
 <head>
-<script type='text/javascript'>
-var websocket_url="<?=$websocket_url?>";
-</script>
 <script type='text/javascript' src='lib/jquery.js'></script>
 <script type='text/javascript' src='client.js'></script>
+<script type='text/javascript'>
+var connection=new Connection("<?=$websocket_url?>");
+function client_send(text) {
+  connection.send({ msg: text });
+}
+</script>
 </head>
 <body>
+<input type='button' onclick='client_send("foobar")' value='send'>
 </body>
 </html>
