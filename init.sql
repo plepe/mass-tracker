07e0abbc887921d915d2d139ac96f0b79ebb5c34
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-02-20 07:39:09 +0100

    New db structure, save messages to db

--- init.sql
+++ init.sql
@@ -1,19 +1,4 @@
-create table gps_log (
-  tracker_id	varchar(32)	not null,
-  event_id	integer		null,
-  timestamp	datetime	not null,
-  longitude	float		not null,
-  latitude	float		not null,
-  altitude	float		null,
-  speed		float		null,
-  accuracy	float		null,
-  altitudeAccuracy	float	null,
-  heading	float		null,
-  foreign key(event_id) references event(event_id)
-);
-
 create table mass_event (
-  event_id	integer		primary key,
   name		text		null,
   description	text		null,
   begin_time	datetime	null,
@@ -24,11 +9,10 @@ create table mass_event (
   begin_zoom	integer		null
 );
 
-create table tracker_data (
+create table tracker_message (
+  msg_num	integer		primary key autoincrement,
   tracker_id	varchar(32)	not null,
-  event_id	integer		null,
   timestamp	datetime	not null,
-  name		text		null,
-  color1	text		null,
-  color2	text		null
+  type		text		not null,
+  data		text		null
 );
