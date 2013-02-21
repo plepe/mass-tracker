5dfd06b863a71755e9053a11912459ca66dc55ae
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-02-21 07:26:25 +0100

    Implement connect-protocol

--- client.js
+++ client.js
@@ -15,13 +15,19 @@ function Connection(url) {
   this.messages=new Messages();
 
   this.connection.onopen=function() {
-    alert("onopen called");
+    this.conf=this.get_cookie("mass_tracker");
 
-    this.send("Test message");
-  }
+    var msg={
+      client_id: this.conf.client_id
+    };
+
+    this.send(msg, 'hello');
+  }.bind(this);
 
   this.connection.onmessage=function(message) {
     var param=JSON.parse(message.data);
+    console.log('RECEIVE');
+    console.log(param);
 
     if(param.ack) {
       for(var i=0; i<this.to_send.length; i++) {
@@ -38,6 +44,14 @@ function Connection(url) {
     }
     else {
       alert("message received:\n"+JSON.stringify(param, null, "  "));
+
+      if(param.type=='welcome') {
+	this.peer_id=param.peer_id;
+	this.client_id=param.data.client_id;
+
+	this.conf.client_id=this.client_id;
+	this.set_cookie('mass_tracker', this.conf);
+      }
     }
 
     this.messages.receive(param);
@@ -58,8 +72,34 @@ Connection.prototype.send=function(data, type) {
 
   this.to_send.push(param);
 
+  console.log('SEND');
+  console.log(param);
   this.connection.send(JSON.stringify(param));
 }
 
 Connection.prototype.check_to_send=function() {
+}
+
+Connection.prototype.get_cookie=function(k) {
+  if(document.cookie) {
+    var cookies=document.cookie.split(/;/);
+
+    for(var i=0; i<cookies.length; i++) {
+      var m;
+
+      if(m=cookies[i].match(/ *([A-Za-z0-9_]+)=(.*)$/)) {
+	if(m[1]==k)
+	  return JSON.parse(m[2]);
+      }
+    }
+  }
+
+  return {};
+}
+
+Connection.prototype.set_cookie=function(key, value) {
+  var expiry=new Date();
+  expiry.setTime(expiry.getTime()+365*86400000);
+
+  document.cookie=key+"="+JSON.stringify(value)+"; expires="+expiry.toGMTString()+"; path=/";
 }
