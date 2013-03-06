feab0b9cab097d0691abf859c24fb302410c9904
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-03-06 16:05:05 +0100

    Format: always check for valid data

--- inc/format.js
+++ inc/format.js
@@ -14,8 +14,15 @@ function format_name(data) {
 function format_icon(data) {
   var ret='img_cycle.php?color1=%color1%&color2=%color2%';
 
-  ret=ret.replace(/%color1%/, urlencode(data.color1));
-  ret=ret.replace(/%color2%/, urlencode(data.color2));
+  var color1="#ffffff";
+  if(data&&(typeof data.color1!="undefined"))
+    color1=data.color1;
+  var color2="#000000";
+  if(data&&(typeof data.color2!="undefined"))
+    color2=data.color2;
+
+  ret=ret.replace(/%color1%/, urlencode(color1));
+  ret=ret.replace(/%color2%/, urlencode(color2));
 
   return ret;
 }
